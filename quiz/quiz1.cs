cfc7cc600e86303ae99e41a99a4bc39ad80902fc
Forrest-Flowers <forrest.w.flowers@gmail.com>
2018-10-03 20:50:20 -0500

    Quiz1

---
+++ quiz/quiz1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name ="";
+
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}!", name);
+        }
+    }
+}
