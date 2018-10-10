d5fc79234e12457315b96701fe644ddcb2474c72
Forrest-Flowers <forrest.w.flowers@gmail.com>
2018-10-10 00:16:23 -0500

    Finished up Practice.

--- week1-practice/Program.cs
+++ week1-practice/Program.cs
@@ -5,34 +5,61 @@ namespace week1_practice
     class Program
     {
         static void Main(string[] args)
-        {   int num1;
+        {   // ints for add function.
+            int num1;
             int num2;
             int total;
 
+            // Write the first number in the problem
             Console.WriteLine("Please enter an integer you would like to add:");
             num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Write the second number.
             Console.WriteLine("And the second integer:");
             num2 = Convert.ToInt32(Console.ReadLine());
             total = num1 + num2;
 
+            //Let's add em up.
+
             Console.WriteLine("{0} + {1} = {2}", num1, num2, total);
 
-            string name = "John";
+            //End of adding program.
 
-            name.ToUpper();
-            Console.WriteLine(name.ToUpper());
+
+            // The two ints we're working with.
+            int yards = 1;
+            int inches = yards * 36;
+
+            // Converts yards to inches. Mission accomplished.
+            Console.WriteLine("{0} yard(s) is equal to {1} inches.", yards, inches);
 
             bool People = true;
             var f = false;
-            var num = 1.7;
-            Console.WriteLine(num * num);
 
-            // string firstName = "Forrest";
-            // string lastName= "Flowers";
-            // string age = "19";
-            // string job = "Amateur Developer"; 
-            // string favoriteBand = "Streetlight Manifesto";
+            // Take var num and multiply it by itself.
+            var num = 1.7m;
+            Console.WriteLine(num * num);
+            // Make var num an integer.
+            int intnum = Convert.ToInt32(num);
+            Console.WriteLine(intnum);
+
+            //Personal Info
+            string firstName = "Forrest";
+            string lastName= "Flowers";
+            string age = "19";
+            string job = "Amateur Developer"; 
+            string favoriteBand = "Streetlight Manifesto";
             // string favoriteSportsTeam = "N/A";
+
+            //Writes out my amazing introduction.
+            Console.WriteLine("Hi, my name is {0} {1} and I am {2} years old. I work as a {3}, and my favorite band is {4}.", firstName, lastName, age, job, favoriteBand);
+
+
+            Console.WriteLine(100 * 10);
+            Console.WriteLine(100 / 10);
+            Console.WriteLine(100 + 10);
+            Console.WriteLine(100 - 10);
+
         }   
     }
 }
