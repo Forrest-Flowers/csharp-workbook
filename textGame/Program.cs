898b071d700deec1b64642642db8bf8720c11877
Forrest-Flowers <forrest.w.flowers@gmail.com>
2018-10-10 17:51:33 -0500

    Started textGame

--- textGame/Program.cs
+++ textGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace textGame
 {
@@ -6,7 +7,20 @@ namespace textGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Welcome to the cavern of secrets!");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Thread.Sleep(2000);
+
+            bool ch1;
+            bool stick = false;
+            String yes =
+
+            Console.WriteLine("You enter a dark cavern out of curiosity. It is dark and you can only make out a small stick on the floor.");
+            Console.WriteLine("Do you pick up the stick? (y/n)");
+            Console.ReadLine();
+            
+            if ()
         }
     }
 }
