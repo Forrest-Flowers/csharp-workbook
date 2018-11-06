5c1c663245e45de2512aff74088269c84ded2910
Forrest-Flowers <forrest.w.flowers@gmail.com>
2018-11-06 13:55:15 -0600

    Skeleton for Mastermind/Towers

--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -6,7 +6,27 @@ namespace TowersOfHanoi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
+    }
+
+    class Game
+    {
+        //make towers
+        // make blocks
+        // setup game (Put blocks on one tower)
+        // Display "Board"
+
+        // Ask for Move.
+        // Ask Tower if move is legal.
+        // Check for win.
+        // Place Block.
+
+
+    }
+
+    class Tower
+    {
+        //Check for legal move.
     }
 }
