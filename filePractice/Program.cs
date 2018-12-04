5acd9ace66174cc212fa0c57c6012c33407beb72
Forrest-Flowers <forrest.w.flowers@gmail.com>
2018-12-03 20:14:40 -0600

    File Practice

---
+++ filePractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace filePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String file = @"test.txt";
+
+          string[] lines = File.ReadAllLines(file);
+
+            int lineNo = 0;
+          foreach(String line in lines)
+          {
+              lineNo += 1;
+              Console.WriteLine(lineNo+" "+line);
+          }
+        
+        string copy = @"copy.txt";
+        File.WriteAllLines(copy, lines);
+
+        }
+    }
+}
